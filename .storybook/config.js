a69424f7933baeb8461fddcf9e4e7fa04f8a2d39
Karl Xu <karl.yogi.xu@gmail.com>
2017-07-12 10:06:52 +0800

    first story

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,10 @@
 import { configure } from '@storybook/react';
 
+const req = require.context('../src', true, /\.stories\.js$/)
+
 function loadStories() {
-  require('../stories');
+    req.keys().forEach((filename) => req(filename))
+    require('../stories');
 }
 
 configure(loadStories, module);
---
+++ src/components/TestStoriesButton/TestStoriesButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestStoriesButton = function() {
+    return (
+        <button>
+            this is a button
+        </button>
+    )
+}
+
+export default  TestStoriesButton
