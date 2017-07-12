cf53e45c5fab00d17b37902e552e7d0f5337634c
Karl Xu <karl.yogi.xu@gmail.com>
2017-07-12 09:35:41 +0800

    first commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function this_test_will_not_explode()
+    {
+        $this->assertTrue(true);
+    }
+}
